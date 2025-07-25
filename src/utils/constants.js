33e33cd197f5390edd1df9ee4bb34914713c51b7
Anjarul04 <haqueanjarul232@gmail.com>
2025-07-25 17:09:53 +0530

    changes in cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,8 @@
 export const IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const RESTAURANT_MENU_API = "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&;complete-menu=true&lat=28.63270&lng=77.21980&restaurantId=";
+export const RESTAURANT_MENU_API = 
+  "https://api.allorigins.win/raw?url=" + 
+  encodeURIComponent("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.63270&lng=77.21980&restaurantId=");
+
 
 export const LOGO_ULR = "https://1000logos.net/wp-content/uploads/2021/05/Swiggy-emblem.png";
